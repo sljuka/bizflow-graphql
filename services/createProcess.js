f6b06e315dfc0cd061acd48ea71baf94b20d62e8
David Sljukic <dsljukic@blueberryapps.com>
2016-04-23 11:16:20 +0200

    Refactor createProcess to be more readable

--- services/createProcess.js
+++ services/createProcess.js
@@ -1,36 +1,40 @@
 import Promise from 'bluebird';
 
 export default function createProcess(Process, processMetaData) {
-  const {name, description} = processMetaData;
+  const {name, description, actions} = processMetaData;
 
   return Process.create({
     name: name,
     description: description
   })
   .then(pcss => {
-    const promises = processMetaData.actions.map((actionItem) => {
-      const {name, description, tasks} = actionItem;
-
-      return pcss.createAction({
-        name: name,
-        description: description
-      })
-      .then(action => {
-        if (!tasks) return Promise.resolve();
-
-        const promisesbre = tasks.map(taskItem => {
-          const {name, description} = taskItem;
-
-          return action.createTask({
-            name: name,
-            description: description
-          });
-        });
-
-        return Promise.all(promisesbre);
-      });
-    });
+    const promises = actions.map(actionMeta => createAction(pcss, actionMeta));
 
     return Promise.all(promises);
+  });
+}
+
+function createAction(pcss, actionMeta) {
+  const {name, description, tasks} = actionMeta;
+
+  return pcss.createAction({
+    name: name,
+    description: description
+  })
+  .then(action => {
+    if (!tasks) return Promise.resolve();
+
+    const promises = tasks.map(taskMeta => createTask(action, taskMeta));
+
+    return Promise.all(promises);
+  });
+}
+
+function createTask(action, taskMeta) {
+  const {name, description} = taskMeta;
+
+  return action.createTask({
+    name: name,
+    description: description
   });
 }
