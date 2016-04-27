81204591b492f5eebe9bfff6f71817e7465b4578
David Sljukic <dsljukic@blueberryapps.com>
2016-04-27 16:23:45 +0200

    Change service to use map instead of for of

--- src/services/createProcessInstance.js
+++ src/services/createProcessInstance.js
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize';
 import Db from '../db';
+import Promise from 'bluebird';
 
 export default function createProcessInstance(Process, pcssId, userId, additionalInfo) {
   return Db.transaction({type: Sequelize.Transaction.EXCLUSIVE}, async function() {
 
     const dbProcess = await Process.findById(pcssId);
-
     const {name, description} = dbProcess.dataValues;
 
     const dbProcessInstance = await dbProcess.createProcessInstance({
@@ -17,10 +17,10 @@ export default function createProcessInstance(Process, pcssId, userId, additiona
 
     const dbActions = await dbProcess.getActions();
 
-    for (const dbAction of dbActions) {
+    const actionPromises = dbActions.map(async function(dbAction) {
       const {id: actionId, type, name, description} = dbAction.dataValues;
 
-      const actionInstance = await dbProcessInstance.createActionInstance({
+      const dbActionInstance = await dbProcessInstance.createActionInstance({
         actionId,
         type,
         name,
@@ -28,42 +28,19 @@ export default function createProcessInstance(Process, pcssId, userId, additiona
       });
 
       const dbTasks = await dbAction.getTasks();
-      if (dbTasks.length === 0) continue;
-
-      for (const dbTask of dbTasks) {
+      const taskPromises = dbTasks.map(async function(dbTask) {
         const { id: taskId, name, description } = dbTask.dataValues;
 
-        await actionInstance.createTaskInstance({
+        await dbActionInstance.createTaskInstance({
           taskId,
           name,
           description
         });
-      }
-    }
+      });
+
+      await Promise.all(taskPromises);
+    });
+
+    await Promise.all(actionPromises);
   });
 }
-
-// async function createActionInstance(dbProcessInstance, dbActionMeta) {
-//   const {id: actionId, type, name, description} = dbActionMeta.dataValues;
-//
-//   const actionInstance = await dbProcessInstance.createActionInstance({
-//     actionId,
-//     type,
-//     name,
-//     description
-//   });
-//
-//   const dbTasks = await dbActionMeta.getTasks();
-//
-//   const promises = dbTasks.map(async function(dbTask) {
-//     const { id: taskId, name, description } = dbTask.dataValues;
-//
-//     await actionInstance.createTaskInstance({
-//       taskId,
-//       name,
-//       description
-//     });
-//   });
-//
-//   return Promise.all(promises);
-// }
