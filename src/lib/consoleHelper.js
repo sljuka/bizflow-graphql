cb973f9f92ed456a2f167baa8fa343e026be467f
David Sljukic <dsljukic@blueberryapps.com>
2016-11-13 14:26:38 +0100

    Refactor the terminal color messages functions

--- src/lib/consoleHelper.js
+++ src/lib/consoleHelper.js
@@ -1,9 +1,9 @@
 const colors = require('colors');
 
 module.exports.printError = function printError(message) {
-  console.log(colors.red('\nErrr:'), colors.red(message), '\n'); // eslint-disable-line no-console
+  console.log(colors.red(`\nErrr: ${message}\n`)); // eslint-disable-line no-console
 };
 
 module.exports.printSuccess = function printSuccess(message) {
-  console.log(colors.green('\nHOORA:'), colors.green(message), '\n'); // eslint-disable-line no-console
+  console.log(colors.green(`\nHOORA: ${message}\n`)); // eslint-disable-line no-console
 };
